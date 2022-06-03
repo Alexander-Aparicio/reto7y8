b7cf3436eabb92dcab1e8a9b4a97791c03456ebd
GiancarloMT <giancarlomt@gmail.com>
2022-06-02 22:47:13 -0500

    update gallery and card css

--- src/components/main/gallery/card/Card.js
+++ src/components/main/gallery/card/Card.js
@@ -1,25 +1,24 @@
-import './Card.css';
+import "./Card.css";
 
-const Card = ({img,age, setInfo, id})=> {
+const Card = ({ img, age, setInfo, id }) => {
+  const handleClick = () => {
+    setInfo(id);
+    window.scrollTo(0, 200);
+  };
 
-    const handleClick=()=>{
-        setInfo(id)
-        window.scrollTo(0,200)
-    }
+  return (
+    <figure className="figure">
+      <img
+        className="figure__image"
+        src={img}
+        alt="card de película"
+        onClick={handleClick}
+      />
+      <figcaption className="figure__caption">
+        <p>{age}</p>
+      </figcaption>
+    </figure>
+  );
+};
 
-    return(
-        <figure className="figure flex flex-col flex-wrap w-36 sm:w-48 lg:w-52 xl:w-56 rounded-lg delay-100 cursor-pointer overflow-hidden border-transparent hover:border-red-600 border-2">
-            <img 
-            className="figure__image w-full rounded-t-lg"
-            src={img}  
-            alt="card de película"
-            onClick={handleClick}
-            />
-            <figcaption className="figure__caption flex w-full justify-center py-2 bg-gray-800">
-                <p className="text-white">{age}</p>
-            </figcaption>
-        </figure>
-    )
-}
-
-export default Card
+export default Card;
