ac7002281d9037c8c1a6252e35f9c8420400221f
Alexander-Aparicio <alexdel2022@gmail.com>
2022-06-10 23:13:47 -0500

    agregando 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { DataProvider } from "./context/DataContext";
 import Buscador from "./pages/Buscador";
+import Error404 from "./pages/Error404";
 import Home from "./pages/Home";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/buscador" element={<Buscador />} />
+        <Route path="/*" element={<Error404 />} />
       </Routes>
     </DataProvider>
   );
---
+++ src/pages/Error404.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Error404.module.css";
+
+const Error404 = () => {
+  return (
+    <>
+      <div className={styles.page}>
+        <h1 className={styles.title}>Error 404</h1>
+      </div>
+    </>
+  );
+};
+
+export default Error404;
